e7a6b3cd88d8c6cfce0f23ee4795056c9a3b06c0
robzor92 <robin.eric.andersson@gmail.com>
2018-08-13 13:52:22 +0200

    remove program

--- providers/start.rb
+++ providers/start.rb
@@ -214,9 +214,6 @@ indexes_installed = "#{node['elastic']['home_dir']}/.indexes_installed"
            "hopsworks":{
              "type":"keyword"
            },
-           "program":{
-             "type":"keyword"
-           },
            "logdir":{
              "type":"keyword"
            },
